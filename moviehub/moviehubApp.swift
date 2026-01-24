db421d221cf650c0aa14607d46cce2a3c33bd838
Gleb Ustymenko <gleb.ustimenko@gmail.com>
2026-01-24 11:46:58 +0100

    Initial Commit

---
+++ moviehub/moviehubApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviehubApp.swift
+//  moviehub
+//
+//  Created by GlebUs on 24/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct moviehubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
